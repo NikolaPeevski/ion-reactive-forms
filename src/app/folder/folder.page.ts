b2b15287d74b67d3f166fdf513c7ceb6052e17de
nikolapeevski <Jeratull@hotmail.com>
2020-06-15 16:41:54 -0500

    Init commit

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,18 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { FormGroup, FormControl } from "@angular/forms";
 
 @Component({
-  selector: 'app-folder',
-  templateUrl: './folder.page.html',
-  styleUrls: ['./folder.page.scss'],
+  selector: "app-folder",
+  templateUrl: "./folder.page.html",
+  styleUrls: ["./folder.page.scss"],
 })
 export class FolderPage implements OnInit {
   public folder: string;
+  public registerForm: any;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute) {}
 
   ngOnInit() {
-    this.folder = this.activatedRoute.snapshot.paramMap.get('id');
+    this.registerForm = new FormGroup({
+      email: new FormControl(),
+      password: new FormControl(),
+    });
+    this.folder = this.activatedRoute.snapshot.paramMap.get("id");
   }
 
+  onRegisterUser() {
+    console.log(
+      `Email: ${this.registerForm.value.email} Password: ${this.registerForm.value.password}`
+    );
+  }
 }
